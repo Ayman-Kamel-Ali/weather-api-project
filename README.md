077599252f0bc7a6609ab316d892ecaf7fb1d3d2
ayman_kamel <ayman_20210202@fci.helwan.edu.eg>
2025-12-10 22:01:32 +0200

    Initial commit - weather API project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Weather Data Collector (Python)
+
+This project collects real-time weather data (temperature & humidity) using the Open-Meteo API.
+The script:
+- Converts city name → coordinates
+- Calls the weather API
+- Extracts temperature & humidity
+- Saves data to CSV
+
+## How to run
+
+pip install -r requirements.txt
+python main.py
---
+++ main.py
@@ -0,0 +1,39 @@
+from geopy.geocoders import Nominatim
+import requests
+import csv
+
+locator = Nominatim(user_agent="my_app")
+city = "Cairo"
+
+try:
+    location = locator.geocode(city)
+    if location is None:
+        raise ValueError("City not found")
+
+    lat = location.latitude # type: ignore
+    long = location.longitude # type: ignore
+
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": lat,
+        "longitude": long,
+        "current": "temperature_2m,relative_humidity_2m",
+        "timezone": "auto"
+    }
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+
+    data = response.json()
+    time = data["current"]["time"].split("T")[0]
+    temp = data["current"]["temperature_2m"]
+    humidity = data["current"]["relative_humidity_2m"]
+
+    print(f"{time} | {city} | {temp}°C | {humidity}%")
+
+    with open("weather_data.csv", "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([time, city, temp, humidity])
+
+except Exception as e:
+    print("error:", e)
